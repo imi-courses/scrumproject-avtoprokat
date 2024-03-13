ede4f5271cf9758c04b78cef0c4cbea0e3887906
romanov-ua <72062394+romanov-ua@users.noreply.github.com>
2024-03-13 16:21:46 +0900

    routes setup

--- avtoprokat/server/index.js
+++ avtoprokat/server/index.js
@@ -4,10 +4,12 @@ const sequelize = require('./db')
 const models = require('./models/models') 
 const PORT = process.env.PORT || 5000;
 const cors = require('cors')
+const router = require('./routes/index')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use('/api',router)
 
 app.get('/',(req,res)=>{
     res.status(200).json({message:'nice'})
